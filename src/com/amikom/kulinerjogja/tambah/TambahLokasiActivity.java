2eb8b1ba32b5c14145a994c79f8b0b7245684b57
Burhan Alfironi Muktamar <burhan@polatic.co.id>
2014-06-17 22:40:16 +0700

    sdfdf

---
+++ src/com/amikom/kulinerjogja/tambah/TambahLokasiActivity.java
@@ -0,0 +1,15 @@
+package com.amikom.kulinerjogja.tambah;
+
+import com.amikom.kulinerjogja.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TambahLokasiActivity extends Activity{
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    // TODO Auto-generated method stub
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.tambah_lokasi_kuliner_layout);
+}
+}
---
+++ src/com/amikom/kulinerjogja/terdekat/KulinerTerdekatActivity.java
@@ -0,0 +1,16 @@
+
+package com.amikom.kulinerjogja.terdekat;
+
+import com.amikom.kulinerjogja.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KulinerTerdekatActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kuliner_terdekat_layout);
+    }
+}
