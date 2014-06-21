e2da9bb8c34287411e135c91f3ef6457a6f7d882
atiatul <atiatul@polatic.co.id>
2014-06-21 12:57:05 +0700

    d

--- src/com/amikom/kulinerjogja/terdekat/KulinerTerdekatActivity.java
+++ src/com/amikom/kulinerjogja/terdekat/KulinerTerdekatActivity.java
@@ -1,18 +1,60 @@
-
 package com.amikom.kulinerjogja.terdekat;
 
 import com.amikom.kulinerjogja.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.TextView;
 
-public class KulinerTerdekatActivity extends Activity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.kuliner_terdekat_layout);
-    }
+public class KulinerTerdekatActivity extends Activity implements
+		OnClickListener {
+	private TextView m100;
+	private TextView m200;
+	private TextView m300;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.kuliner_terdekat_layout);
+		initview();
+	}
+
+	private void initview() {
+		m100 = (TextView) findViewById(R.id.m100);
+		m200 = (TextView) findViewById(R.id.m200);
+		m300 = (TextView) findViewById(R.id.m300);
+		m100.setOnClickListener(this);
+		m200.setOnClickListener(this);
+		m300.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.m100:
+			click100();
+			break;
+		case R.id.m200:
+			click200();
+			break;
+		case R.id.m300:
+			click300();
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void click100() {
+	}
+
+	private void click200() {
+	}
+
+	private void click300() {
+	}
 }
--- src/com/amikom/kulinerjogja/terdekat/ListKulinerTerdekatActivity.java
+++ src/com/amikom/kulinerjogja/terdekat/ListKulinerTerdekatActivity.java
@@ -1,7 +1,12 @@
 
 package com.amikom.kulinerjogja.terdekat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.amikom.kulinerjogja.R;
+import com.amikom.kulinerjogja.model.MakananModel;
+import com.amikom.kulinerjogja.pencarian.PencarianAdapter;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -10,20 +15,25 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListKulinerTerdekatActivity extends Activity {
-    private ListView mList;
-    private ArrayAdapter<String> mAdapter;
-    private final String[] mItem = {
-            "a", "b", "c"
-    };
 
+	private ListView mListView;
+	private List<MakananModel> makananModels;
+	private PencarianAdapter adapter;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_kuliner_terdekat_layout);
-        mList = (ListView) findViewById(R.id.list_kuliner_terdekat);
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mItem);
-        mList.setAdapter(mAdapter);
+        initview();
     }
+    private void initview() {
+		mListView = (ListView) findViewById(R.id.list_kuliner_terdekat);
+		setdata();
+	}
+    private void setdata() {
+		makananModels = new ArrayList<MakananModel>();
+		adapter =  new PencarianAdapter(this, makananModels);
+		mListView.setAdapter(adapter);
+	}
 }
